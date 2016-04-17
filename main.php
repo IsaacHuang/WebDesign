6fbdc435b795f0d4727911ff547b50b65160033b
IsaacHuang <issac11235813@gmail.com>
2016-04-17 10:14:11 +0800

    test google map

---
+++ main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		CYCU_MAP
+	</title>
+	<script type="text/javascript">
+		function initMap() {
+  			// Create a map object and specify the DOM element for display.
+  			var map = new google.maps.Map(document.getElementById('map'), {
+    			center: {lat: -34.397, lng: 150.644},
+    			scrollwheel: false,
+    			zoom: 8
+  			});
+		}
+	</script>
+</head>
+<body>
+	<div id="map">
+		
+	</div>
+</body>
+</html>
